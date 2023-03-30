13dedf1bf2b8d8dbfc7a8529f880c4da20f0011d
ulissayang <ulissleksmart@gmail.com>
2023-03-30 18:24:02 +0900

    update beberapah fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,6 +7,14 @@ hamburger.addEventListener("click", function () {
   navMenu.classList.toggle("hidden");
 });
 
+//klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
 //navbar fixed
 window.onscroll = function () {
   const header = document.querySelector("header");
