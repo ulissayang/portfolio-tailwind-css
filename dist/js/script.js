04649403edd161faa1f36084ebe70de8b4713569
ulissayang <ulissleksmart@gmail.com>
2023-03-30 14:51:23 +0900

    tambahkan dark mode dan beberapah fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,10 +11,39 @@ hamburger.addEventListener("click", function () {
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
   }
 };
+
+// toggle mode dark and white
+const checkbox = document.querySelector("#toggle");
+const html = document.querySelector("html");
+
+checkbox.addEventListener("click", function () {
+  checkbox.checked ? html.classList.add("dark") : html.classList.remove("dark");
+  if (checkbox.checked) {
+    localStorage.theme = "dark";
+  } else {
+    localStorage.theme = "light";
+  }
+});
+
+// pindahkan posisi toggle sesuia mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  checkbox.checked = true;
+} else {
+  checkbox.checked = false;
+}
